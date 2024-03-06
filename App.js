e624e5bbd5a361c734959405a3569ffa9935f6b9
Angel Cruz <136207924+angelcruz07@users.noreply.github.com>
2024-03-06 16:27:23 -0600

    Feat: add funtionality

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import  CatComponent from './components/CatComponent';
+import {Button, StyleSheet, View, Image} from 'react-native';
+import {useEffect, useState} from "react";
+import { Audio } from 'expo-av';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <CatComponent />
+  const image = 'https://www.elgatofeliz.es/wp-content/uploads/2020/12/raza-gato-azul-ruso.gif';
+  const [sound, setSound] = useState();
+  async function playSound() {
+    console.log('Loading Sound');
+    const { sound } = await Audio.Sound.createAsync( require('./assets/sound.mp3')
+    );
+    setSound(sound);
+    console.log('Playing Sound');
+    await sound.playAsync();
+  }
 
-    </View>
+  useEffect(() => {
+    return sound
+        ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync();
+        }
+        : undefined;
+  }, [sound]);
+
+  return (
+      <View style={styles.container}>
+        <Image source={{uri: image}} style={{width: 200, height: 250}}/>
+        <Button title='Escuchar' onPress={playSound}/>
+        <StatusBar style="auto"  />
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
